eb960490da1633c24ac5866e0c2623e568dad4d8
Stéphane Bachelier <stephane.bachelier@gmail.com>
2014-05-22 15:08:55 +0200

    chore(grunt): setup grunt to prepare distribution files
    add minificatio

---
+++ Gruntfile.js
@@ -0,0 +1,79 @@
+/*global require, module  */
+module.exports = function(grunt) {
+
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      ' * Original work from phantom limb by Brian Cartensen\n' +
+      ' * Adapted by @fat and @XhmikosR for github.com/twbs/ratchet\n' +
+      ' * Licensed <%= pkg.license %>\n*/\n\n',
+    // Task configuration.
+    clean: {
+      src: 'dist'
+    },
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: ['lib/<%= pkg.name %>.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        globals: {}
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib: {
+        src: ['lib/*.js']
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib: {
+        files: '<%= jshint.lib.src %>',
+        tasks: ['jshint:lib']
+      }
+    }
+  });
+
+  // Default task.
+  grunt.registerTask('default', ['clean', 'jshint', 'concat', 'uglify']);
+
+};
